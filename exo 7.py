8a55e8f374bf082a6ab31f27ea31dab8ba728b69
samuel71999 <samuel71999@gmail.com>
2018-05-07 14:40:12 +0200

    hey
    salut

--- exo 7.py
+++ exo 7.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  4 14:34:40 2018
+Created on Fri May  4 16:59:24 2018
 
 @author: etudiant
 """
-
 def decaler(caractere, decalage):
     caractere = ord(caractere)
     b = caractere + decalage
     lettre_changee = chr(b)
     return lettre_changee
-    
+
 print(decaler(input('caractere: '),2))
+    
+    
+    
