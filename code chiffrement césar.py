c9fe5b47c6f40c0a888299e8f1194f2fa67bb5dc
samuel71999 <samuel71999@gmail.com>
2018-05-07 17:09:19 +0200

    on avance
    ajout du oui ou non

--- code chiffrement césar.py
+++ code chiffrement césar.py
@@ -19,16 +19,19 @@ for i in range (len(message)) : #len(message) = longueur du message
     tableau.append(lettre_cle) # on incrémente la lettre dans le tableau
     
 phrase_modifiée = "".join(tableau)
-print(phrase_modifiée)
+print('voici le message déchiffré : ',phrase_modifiée)
     
 #le message s'affiche à la verticale alors qu'il faut qu'il soit a l'horizontale
 #attention aux espaces (il faut les laisser)
 #apres la lettre z ou avant la lettre a le code marche pas, car ASCII n'est pas une boucle 
 
-
-
-
-
+a= int(input('le message est-il déchifré ? (si oui tapez 1, si non tapez 0) :'))
+if a==1:
+    print('merci pour votre participation, votre message nous aidera a améliorer notre intelligence artificielle')
+elif a==0:
+    print('nous allons recommencer ...')
+else :
+    print('nous avons affaire a un génie ...')
 
 
 
