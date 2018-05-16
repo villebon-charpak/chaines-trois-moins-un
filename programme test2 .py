9536dbdbaa65aed0f2c77d4046647c77776dff13
lehuiy <lehui.ye@u-psud.fr>
2018-05-16 16:25:33 +0200

    code chiffrement de césar
    partie sur compter occurence dans le message, classer les lettres dans l'odre décroissant en fonction de leur occurence

--- programme test2 .py
+++ programme test2 .py
@@ -17,15 +17,15 @@ for i in range (0,127): # pour i allant de a à z en chiffre, chr fait passer de
 
 tableau_occurence = [] 
 
-while liste != []:
+while liste != [0]*127: # tant que dans la liste il n'y a que des 0, répeter la boucle
     a = liste[0]
     indice = 0
     for i in range (0,len(liste)) : # permet de trouver la lettre le plus repétée
         if a < liste[i] :
             a = liste[i] # nombre de fois ou la lettre est repétée
             indice = i # chiffre correspondant à la lettre        
-            tableau_occurence.append(indice)
-            del liste[indice]
+    tableau_occurence.append(indice) # apporter cet indice dans une nouvelle liste 
+    liste[indice] = 0 # remplacer l'occurence à l'indice i par 0
 
 print(tableau_occurence)
 
